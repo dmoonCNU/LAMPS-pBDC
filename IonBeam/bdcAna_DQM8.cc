a5f255782886fb2a7e1a8f5afa910c00b01e2fd0
Dongho Moon <dmoon@cern.ch>
2022-12-12 16:47:13 +0900

    Update for IonBeam

---
+++ IonBeam/bdcAna_DQM8.cc
@@ -0,0 +1,223 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TGraph.h"
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include "TH1.h"
+#include "TH2.h"
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TSystem.h"
+#include "TStyle.h"
+
+using namespace std;
+
+double ped[64]={179.93,178.4,177.34,180.94,179.93,184.42,187.45,191.72,180.37,177.74,178.27,178.53,180.06,180.85,187.12,193.11,182.03,181.65,180.05,183.17,183.12,187.14,189.48,193.75,182.54,179.7,181.01,180.51,180.14,185.12,187.36,192.65,179.33,175.63,176.17,179.81,178.17,181.02,186.34,192.3,176.93,175.91,175,177.56,175.23,180.1,185.3,185.62,177.88,176.86,178.1,176.7,179.8,183.83,183.07,192.66,181.29,178.66,177.66,177.51,177.12,183.07,182.71,190.8};
+
+int main( int  argc, char* argv[] ){
+  std::cout<< argc << " " << argv[0] << " : " << argv[1] << " initial : " << argv[2] << std::endl; 
+  std::string filename = argv[1];
+  int inevt=atoi(argv[2]);
+  if (inevt<0) inevt=0;
+  std::cout << "Read : " << filename << std::endl; 
+  TApplication* app = new TApplication("app",&argc,argv ); 
+
+  TFile* itf = new TFile(filename.c_str());
+  TTree* itr = (TTree*)itf->Get("DataTree");
+  Int_t EventNum;
+  UShort_t type;
+  UShort_t ver;
+  UShort_t id;
+  UShort_t trigTime;
+  UShort_t dataLen;
+  UInt_t   trigCount;
+  UInt_t   eventNum;
+  UInt_t   spillNum;
+  Int_t    DataLength;  
+  UShort_t Data[128000];
+  Int_t    TrigCount;
+  UShort_t eNum;
+  UShort_t sNum;
+  itr->SetBranchAddress("EventNum" ,&EventNum );
+  itr->SetBranchAddress("type"     ,&type     );
+  itr->SetBranchAddress("ver"      ,&ver      );
+  itr->SetBranchAddress("id"       ,&id       );
+  itr->SetBranchAddress("trigTime" ,&trigTime );
+  itr->SetBranchAddress("dataLen"  ,&dataLen  );
+  itr->SetBranchAddress("trigCount",&trigCount);
+  itr->SetBranchAddress("eventNum" ,&eventNum );
+  itr->SetBranchAddress("spillNum" ,&spillNum );
+
+  itr->SetBranchAddress("DataLength",&DataLength);
+  itr->SetBranchAddress("Data"      ,Data       );
+  itr->SetBranchAddress("TrigCount" ,&TrigCount );
+  itr->SetBranchAddress("eNum"      ,&eNum      );
+  itr->SetBranchAddress("sNum"      ,&sNum      );
+
+  //TCanvas* can = new TCanvas("can","can",800,800);
+  TCanvas* can = new TCanvas("can","can",1200,800);
+  can->Divide(3,2);
+
+  TGraph* grQDC[64];
+  TGraph* grTDC[64];
+  for( int i = 0; i< 64; i++){
+    grQDC[i] = new TGraph();
+    grTDC[i] = new TGraph();
+    grQDC[i]->SetMarkerStyle(i%8+20);
+    grTDC[i]->SetMarkerStyle(i%8+20);
+    grQDC[i]->SetMarkerColor(i/8+20);
+    grTDC[i]->SetMarkerColor(i/8+20);
+  }
+  /*
+     TH2D* hisQDC = new TH2D("hisQDC","hisQDC;time;ch",64,0,64,64,0,64);  
+     TH1D* hisQDCSum = new TH1D("hisQDCSum","hisQDCSum;ch",64,0,64);
+     TH2D* hisTDC = new TH2D("hisTDC","hisTDC;time;ch",64,0,64,64,0,64); 
+     TH2D* hisWave= new TH2D("hisWave","hisWave;time;QDC",64,0,64,400,-200,200);
+     */
+  TH2D* hisQDC = new TH2D("hisQDC","hisQDC;time;ch",128,0,128,64,0,64);  
+  TH1D* hisQDCSum = new TH1D("hisQDCSum","hisQDCSum;ch",64,0,64);
+  //###TH2D* hisTDC = new TH2D("hisTDC","hisTDC;time;ch",20,40,60,64,0,64); 
+  TH2D* hisTDC = new TH2D("hisTDC","hisTDC;time;ch",128,0,128,64,0,64); 
+  //###TH2D* hisWave= new TH2D("hisWave","hisWave;time;QDC",20,40,60,800,0,800);
+  //######    TH2D* hisWave= new TH2D("hisWave","hisWave;time;QDC",128,0,128,800,0,800);
+  TH2D* hisWave= new TH2D("hisWave","hisWave;time;QDC",128,0,128,600,-100,500);
+  TH2D* hisXYs = new TH2D("hisXYs","hisXYs;Ch X;Ch Y",33,0,33,33,0,33);
+  TH1D* hisTDCDis = new TH1D("hisTDCDis",";time (ns); Counts",300,1850,2150);
+  //TH2D* hisWave= new TH2D("hisWave","hisWave;time;QDC",128,0,128,800,0,800);
+  //TH2D* hisWave= new TH2D("hisWave","hisWave;time;QDC",128,0,128,1000,-200,800);
+
+
+  std::cout << "itr->GetEntries() : " << itr->GetEntries() << std::endl;
+  std::cout << "inevt : " << inevt << std::endl;
+
+  for( int ievt = inevt; ievt < itr->GetEntries(); ievt++){
+    itr->GetEntry(ievt);
+    hisQDC->Reset();
+    hisTDC->Reset();
+    hisWave->Reset();
+    hisQDCSum->Reset();
+    hisXYs->Reset();
+    hisTDCDis->Reset();
+
+    Int_t nPoints = DataLength / 128;
+    Double_t QDCSum[64]={0};
+    //std::cout<< "point" << std::endl;
+    std::cout<< "Event : " << ievt << " !!!"<<std::endl;
+    int firedch1[20] = {0};
+    int firedch2[20] = {0};
+    int cnt1 = 0;
+    int cnt2 = 0;
+    for( int ich = 0; ich < 64; ich++){
+      //           if (ievt%64!=ich) continue;
+      //###                for (int ich = 38; ich < 39; ich++) {
+      //if(ich == 0 || ich == 31 || ich == 32 || ich == 63) continue;
+      for( int ip = 0; ip < nPoints; ip++){
+
+        double tmpQDC=Data[ip*128+ich]-ped[ich]*100/nPoints;
+        unsigned short ftt
+          = ((trigTime & 0xFF00 ) >> 8 )
+          + ((trigTime & 0x007F ) << 8 );
+
+        //###if (tmpQDC > 280){
+        //if(!(ip>44 && ip<48)) continue; 
+        //if(!(ip>44 && ip<48)) continue; 
+        hisQDC->Fill(ip,ich,tmpQDC);
+        hisTDC->Fill(ip,ich,Data[ip*128+64+ich]);
+
+        //hisWave->Fill(ip,Data[ip*128+ich]-Data[ich]);
+        hisWave->Fill(ip,tmpQDC);
+        //###                    QDCSum[ich] += tmpQDC-280;
+        //###                    QDCSum[ich] += tmpQDC-ped[ich]*100/nPoints;
+        QDCSum[ich] += tmpQDC;
+        //      if(ich < 32 && ip>44 && ip<48) {
+        if(Data[ip*128+64+ich] > 0 ) {
+          firedch1[cnt1] = ich;
+          double time1 = (ftt - (Data[ip*128+64+ich]&0x7FFF));
+          if(time1 < 0) time1 = time1+32767;
+          cnt1++;
+        }
+        //      }
+        //###                    if(ich >= 32 && ip>44 && ip<48) {
+        if(Data[ip*128+64+ich] > 0 ) {
+          firedch2[cnt2] = ich;
+          double time2 = (ftt - (Data[ip*128+64+ich]&0x7FFF));
+          if(time2 < 0) time2 = time2+32767;
+          //if(time2 < 0) cout<<"Minus Time ??? ftt : "<<ftt<<", TDC 2 : "<<(Data[ip*128+64+ich]&0x7FFF)<<endl;
+          cnt2++;
+        }
+        //###                    }
+        if( (Data[ip*128+64+ich] &0x7FFF)  != 0 ){
+          double time = (ftt - (Data[ip*128+64+ich]&0x7FFF));
+          if(time < 0) time = time + 32767;
+          hisTDCDis->Fill(time);
+        }
+
+        //###}
+      }
+      hisQDCSum->Fill(ich, QDCSum[ich]);
+    }
+
+    cout<<"# of ch 1 : "<<cnt1<<", # of ch 2 : "<<cnt2<<endl;
+    for(int ich1 = 0; ich1 < cnt1; ich1++){ 
+      for(int ich2 = 0; ich2 < cnt2; ich2++){
+        //cout<<"ich1 : "<<firedch1[ich1]<<", ich2 : "<<firedch2[ich2]-32<<endl;
+        //if(firedch1[ich1] == 0 || firedch2[ich2] == 0) continue;
+        //if(firedch1[ich1] == 30 || firedch2[ich2] == 63) continue;
+        hisXYs->Fill(firedch1[ich1],(31-(firedch2[ich2]-32)));
+      }
+    }
+    cout<<"# of 2D Hits : "<<hisXYs->GetEntries()<<endl;
+
+
+    //std::cout << "QCDSum : " << QDCSum[52] << std::endl;//21
+    /*
+       for (int j=0;j<64;j++) {
+       std::cout << j << " - " << hisQDCSum->GetBinContent(j+1)/100 << std::endl;
+       }
+       */
+    gStyle->SetPalette(kRainBow);
+    can->cd(1);
+    gPad->SetGridx();
+    gPad->SetGridy();    
+    gPad->SetLogz(0);
+    double zmax = hisQDC->GetMaximum();
+    double zmin = hisQDC->GetMinimum();
+    hisQDC->GetZaxis()->SetRangeUser(zmin,zmax);
+
+    hisQDC->Draw("colz");
+    can->cd(2);
+    gPad->SetGridx();
+    gPad->SetGridy();
+    hisTDC->Draw("colz");
+    can->cd(3);
+    gPad->SetGridx();
+    gPad->SetGridy();
+    hisXYs->Draw("colz");
+    can->cd(4);
+    gPad->SetGridx();
+    gPad->SetGridy();
+    hisQDCSum->Draw("HIST");
+    can->cd(5);
+    gPad->SetGridx();
+    gPad->SetGridy();
+    hisWave->Draw("colz");
+
+    can->cd(6);
+    gPad->SetGridx();
+    gPad->SetGridy();
+    gPad->SetLogy();
+    hisTDCDis->Draw("E");
+
+    can->Modified();
+    can->Update();
+    gSystem->ProcessEvents();
+    getchar(); 
+    }
+
+    app->Run();
+
+}
---
+++ IonBeam/bdcDrawAll_KOMAC.C
@@ -0,0 +1,255 @@
+#include "TFile.h"
+#include "TROOT.h"
+#include "TTree.h"
+#include "TGraph.h"
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TSystem.h"
+#include "TStyle.h"
+#include "TLatex.h"
+
+using namespace std;
+
+void set_plot_style()
+{
+  const Int_t NRGBs = 5;
+  const Int_t NCont = 255;
+
+  Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+  Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+  Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+  Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+  TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+  gStyle->SetNumberContours(NCont);
+}
+
+void bdcDrawAll_KOMAC(){
+  gROOT->Macro("~/rootlogon.C");
+  gStyle->SetOptStat(0);
+  gStyle->SetPadRightMargin(0.1);
+
+  int RunNo = 5014;
+  int ASDNo = 0;
+
+  TFile* itf = new TFile(Form("ASD_%d_%d.root",RunNo,ASDNo)); // 460
+  //TFile* itf = new TFile(Form("220526_Cosmic_c1430_a1430_0C00_evt30k_PMTCoin_Delay180ns_expset_aBDCaASD_t106_ASD%d.root",ASDNo));
+  //TFile* itf = new TFile("220526_Cosmic_c1430_a1430_0C00_evt30k_PMTCoin_Delay180ns_expset_aBDCaASD_t106_ASD16.root");
+  TTree* itr = (TTree*)itf->Get("DataTree");
+  Int_t EventNum;
+  UShort_t type;
+  UShort_t ver;
+  UShort_t id;
+  UShort_t trigTime;
+  UShort_t dataLen;
+  UInt_t   trigCount;
+  UInt_t   eventNum;
+  UInt_t   spillNum;
+  Int_t    DataLength;  
+  UShort_t Data[128000];
+  Int_t    TrigCount;
+  UShort_t eNum;
+  UShort_t sNum;
+  itr->SetBranchAddress("EventNum" ,&EventNum );
+  itr->SetBranchAddress("type"     ,&type     );
+  itr->SetBranchAddress("ver"      ,&ver      );
+  itr->SetBranchAddress("id"       ,&id       );
+  itr->SetBranchAddress("trigTime" ,&trigTime );
+  itr->SetBranchAddress("dataLen"  ,&dataLen  );
+  itr->SetBranchAddress("trigCount",&trigCount);
+  itr->SetBranchAddress("eventNum" ,&eventNum );
+  itr->SetBranchAddress("spillNum" ,&spillNum );
+
+  itr->SetBranchAddress("DataLength",&DataLength);
+  itr->SetBranchAddress("Data"      ,Data       );
+  itr->SetBranchAddress("TrigCount" ,&TrigCount );
+  itr->SetBranchAddress("eNum"      ,&eNum      );
+  itr->SetBranchAddress("sNum"      ,&sNum      );
+
+  TCanvas* can = new TCanvas("can","can",0,66,1400,700);
+  //TCanvas* can = new TCanvas("can","can",0,66,1000,600);
+  can->Divide(4,2);
+
+  TH2D* hisQDC1= new TH2D("hisQDC1","hisQDC1;time;QDC",100,0,100,800,200,800);
+  TH2D* hisQDC2= new TH2D("hisQDC2","hisQDC2;time;QDC",100,0,100,800,200,800);
+  TH1D* hisTDC1 = new TH1D("hisTDC1" ,"hisTDC1;channel;TDC Hit",64,0,64);
+  TH1D* hisTDC2 = new TH1D("hisTDC2" ,"hisTDC2;channel;TDC Hit",64,0,64);
+
+  TH2D* hNHitsCh1 = new TH2D("hNHitsCh1",";channel;# of hits",64,0,64,100,0,1000);
+  TH2D* hNHitsCh2 = new TH2D("hNHitsCh2",";channel;# of hits",64,0,64,10,0,10);
+
+  TH1D* hNHits1 = new TH1D("hNHits1",";# of hits;Counts",10,0,10);
+  TH1D* hNHits2 = new TH1D("hNHits2",";# of hits;Counts",10,0,10);
+
+  TH2D* hChXY1 = new TH2D("hChXY1",";X;X'",32,0,32,32,0,32);
+  TH2D* hChXY2 = new TH2D("hChXY2",";X;X'",32,0,32,32,0,32);
+
+  TH1D* hQDC1 = new TH1D("hQDC1",";QDC;Counts",500,0,1000);
+  TH1D* hIpTime = new TH1D("hIpTime",";ip;Counts",120,0,120);
+  TH1D* hIpTimeQDC = new TH1D("hIpTimeQDC",";ip;QDC",120,0,120);
+  TH2D* hIpTimeQDC2 = new TH2D("hIpTimeQDC2",";ip;QDC",120,0,120,500,0,1000);
+
+  TLatex *lt1 = new TLatex();
+  lt1->SetNDC();
+
+  int Eff1=0;
+  int Eff=0;
+  int lastTDC1=0;
+  int nHits1 = 0;
+  int nHits2 = 0;
+  int cnt1 = 0;
+  int cnt2 = 0;
+  int ch11 = 0;
+  int ch12 = 0;
+  int ch21 = 0;
+  int ch22 = 0;
+  int minip = 91;
+  int maxip = 94;
+  double QDCcut = 320; //460; // 300 : 0800 ASD 16, 360 : 0C00 ASD 16
+  //double QDCcut = 600; //460; // 300 : 0800 ASD 16, 360 : 0C00 ASD 16
+  double QDCMax = 560;
+  for( int ievt = 0; ievt < itr->GetEntries(); ievt++){
+    itr->GetEntry(ievt);
+    //if(ievt == 10) break;
+
+    //cout<<"DataLength : "<<DataLength<<endl;
+    Int_t nPoints = DataLength / 128;
+    //cout<<"nPoints : "<<nPoints<<endl;
+    bool isFired1=false;
+    bool isFired=false;
+    for( int ich = 0; ich < 64; ich++){
+      //if((ich > 30 && ich < 34)) continue;
+      //if((ich > 30 && ich < 33)) continue;
+      for( int ip = 0; ip < nPoints; ip++){
+        double tmpQDC=Data[ip*128+ich];
+        //if(!(tmpQDC > 330)) continue;
+
+        if ( Data[ip*128+64+ich] > 0) hisQDC1->Fill(ip,tmpQDC);
+        if ( Data[ip*128+64+ich] > 0) {
+          hQDC1->Fill(tmpQDC);
+          if(tmpQDC > QDCcut) hIpTime->Fill(ip);
+          if(tmpQDC > QDCcut) hIpTimeQDC2->Fill(ip,tmpQDC);
+          //if(tmpQDC > QDCcut) hIpTimeQDC->SetBinContent(ip,tmpQDC);
+          if(tmpQDC> 0) hisTDC1->Fill( ich );
+          if(ich < 32){
+            ch11 = ich;
+          }else{
+            ch12 = ich-31;
+          } 
+          if ((ievt-lastTDC1)<=10) lastTDC1=ievt;
+          isFired1=true;
+          cnt1++;
+        }
+
+        if (tmpQDC > QDCcut){ 
+          if(tmpQDC > QDCMax) continue;
+          if(!(ip > minip && ip < maxip)) continue;
+
+          if (Data[ip*128+64+ich] > 0) { // when TDC exists
+            
+            hisQDC2->Fill(ip,tmpQDC);
+            if(Data[ip*128+64+ich] > 0 ) {
+              if(ich < 32){
+                ch21 = ich;
+              }else{
+                ch22 = ich-31;
+              } 
+              hisTDC2->Fill( ich );
+              isFired=true;
+              cnt2++;
+            }
+          }
+        }
+      }
+
+    }
+    hChXY1->Fill(ch11, ch12);
+    hChXY2->Fill(ch21, ch22);
+    nHits1=cnt1;
+    nHits2=cnt2;
+    cnt1 = 0;
+    cnt2 = 0;
+    //cout<<"nHits 1 : "<<nHits1<<", nHits 2 : "<<nHits2<<endl;
+    //hNHitsCh1->Fill(ich,nHits1);
+    //hNHitsCh2->Fill(ich,nHits2);
+    if (isFired1) Eff1++;
+    if (isFired) Eff++;
+    hNHits1->Fill(nHits1);
+    hNHits2->Fill(nHits2);
+  }
+
+  std::cout << "Num. of Trig. : " << itr->GetEntries() << std::endl;
+  std::cout << "Entry Num. of TDC1: " << hisTDC1->GetEntries() << std::endl;
+  std::cout << "Entry Num. of TDC2: " << hisTDC2->GetEntries() << std::endl;
+  std::cout << "Num. of triggered events (TDC1 fired) " << Eff1 << std::endl;
+  std::cout << "Num. of triggered events (TDC2 fired) " << Eff << std::endl;
+  std::cout << "Last TDC1 fired event (TDC1 fired) " << lastTDC1 << std::endl;
+  std::cout << "Efficiency 1 : " << (double) Eff / itr->GetEntries() << std::endl;
+  std::cout << "Efficiency 2 : " << (double) Eff1 / itr->GetEntries() << std::endl;
+
+  //set_plot_style();
+  gStyle->SetCanvasPreferGL(1);
+  //gStyle->CreateGradientColorTable();
+
+  can->cd(1);
+  gPad->SetLogz();
+  hisQDC1->Draw("colz");
+  lt1->DrawLatex(0.2, 0.9, "HV C-1420 A-1380 at ASD 16 DAC : 0C00");
+  //lt1->DrawLatex(0.2, 0.9, "HV C-1420 A-1420 at ASD 16 DAC : 0800");
+  //lt1->DrawLatex(0.2, 0.9, "HV C-1415 A-1375 at ASD 18");
+
+  can->cd(2);
+  hisTDC1->Draw("E");
+
+  can->cd(3);
+  hNHits1->Draw("E");
+  lt1->DrawLatex(0.6, 0.80, Form("Mean : %0.2f", hNHits1->GetMean()));
+
+  can->cd(4);
+  hChXY1->Draw("colz");
+
+  can->cd(5);
+  gPad->SetLogz();
+  hisQDC2->Draw("colz");
+  lt1->DrawLatex(0.2, 0.17, "After removing pedstal");
+
+  can->cd(6);
+  hisTDC2->Draw("E");
+  lt1->DrawLatex(0.2, 0.17, "After removing pedstal");
+
+  can->cd(7);
+  hNHits2->Draw("E");
+  lt1->DrawLatex(0.4, 0.9, "After removing pedstal");
+  lt1->DrawLatex(0.6, 0.80, Form("Mean : %0.2f", hNHits2->GetMean()));
+
+  can->cd(8);
+  hChXY2->Draw("colz");
+
+  can->SaveAs("220526_Cosmic_c1430_a1430_0C00_evt30k_t106_ASD19.png");
+  //can->SaveAs("220526_Cosmic_c1430_a1430_0C00_evt30k_t106_ASD18.png");
+  //can->SaveAs("220526_Cosmic_c1430_a1430_0C00_evt30k_t106_ASD17.png");
+  //can->SaveAs("220526_Cosmic_c1430_a1430_0C00_evt30k_t106_ASD16.png");
+
+
+  TCanvas *c1 = new TCanvas("c1","",1200,400);
+  c1->Divide(3,1);
+  c1->cd(1);
+  hQDC1->Draw();
+  c1->cd(2);
+  hIpTime->Draw();
+  c1->cd(3);
+  hIpTimeQDC2->Draw("colz");
+  //hIpTimeQDC->Draw();
+
+  c1->SaveAs(Form("komac_total_%d_%d.png",RunNo,ASDNo));
+
+
+}
