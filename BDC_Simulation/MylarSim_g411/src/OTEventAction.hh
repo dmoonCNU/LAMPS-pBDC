c068e2ec19529f6c2009fde8ffe36b671dccca53
Dongho Moon <dmoon@cern.ch>
2023-01-28 17:48:15 +0900

    Geant4 11 for Mac

---
+++ BDC_Simulation/MylarSim_g411/src/OTEventAction.hh
@@ -0,0 +1,26 @@
+#ifndef OTEVENTACTION_HH
+#define OTEVENTACTION_HH
+
+#include "G4UserEventAction.hh"
+#include "G4Event.hh"
+#include "globals.hh"
+//#include "g4root.hh"
+#include "G4AnalysisManager.hh"
+
+class OTEventAction : public G4UserEventAction
+{
+  public:
+    OTEventAction();
+    virtual ~OTEventAction();
+
+    // method from the base class
+    virtual void BeginOfEventAction(const G4Event *);
+    virtual void EndOfEventAction(const G4Event *);
+
+    void AddEnergyDeposit1(G4double edep) { edep1 += edep; }
+
+  private:
+    G4double edep1;
+};
+
+#endif
